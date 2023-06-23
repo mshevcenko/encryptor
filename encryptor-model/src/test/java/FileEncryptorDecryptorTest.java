5e081303f4ccbfd003e2865d7074a8ca9d180518
Zeltronick <muhailo11111@gmail.com>
2023-06-23 08:24:23 +0300

    update FileEncryptorDecryptorTest

--- encryptor-model/src/test/java/FileEncryptorDecryptorTest.java
+++ encryptor-model/src/test/java/FileEncryptorDecryptorTest.java
@@ -1,14 +1,22 @@
-import org.junit.Assert;
 import org.junit.Test;
-
 import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertArrayEquals;
+
 
 public class FileEncryptorDecryptorTest {
 
     @Test
-    public void encryptTest() {
+    public void encryptTest() throws IOException {
         File file = new File("src/main/resources/testFile.txt");
-        Assert.assertFalse(!file.exists());
+        byte[] bytes = DataSaverLoader.load(file);
+        FileEncryptorDecryptor fed = new FileEncryptorDecryptor(file, "1234567890123456", false);
+        fed.encrypt();
+        fed.save(new File("src/main/resources"));
+        FileEncryptorDecryptor fed2 = new FileEncryptorDecryptor(new File("src/main/resources/testFile"), "1234567890123456", true);
+        fed2.decrypt();
+        assertArrayEquals(bytes, fed2.getLoadedFile());
     }
 
 }
